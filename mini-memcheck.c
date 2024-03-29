82c3a21bb77a4362878bf6cea9c334e5a0fec54c
Bryth <62302815+Brythzz@users.noreply.github.com>
2024-03-29 18:41:07 +0100

    Fix realloc

--- mini-memcheck.c
+++ mini-memcheck.c
@@ -67,20 +67,19 @@ void *mini_realloc(void *ptr, size_t request_size, const char *filename,
     return NULL;
   }
 
-  void *new_ptr = realloc(ptr, request_size);
-  if (new_ptr != NULL) {
-    meta_data *meta = new_ptr - sizeof(meta_data);
-
-    int diff = request_size - meta->request_size;
-    meta->request_size = request_size;
-    meta->filename = filename;
-    meta->instruction = instruction;
+  meta_data *prev_meta = ptr - sizeof(meta_data);
+  meta_data *new_meta = realloc(prev_meta, request_size + sizeof(meta_data));
+  if (new_meta != NULL) {
+    int diff = request_size - new_meta->request_size;
+    new_meta->request_size = request_size;
+    new_meta->filename = filename;
+    new_meta->instruction = instruction;
 
     if (diff > 0) total_memory_requested += diff;
     else total_memory_freed += -diff;
   }
 
-  return new_ptr;
+  return (void *)new_meta + sizeof(meta_data);
 }
 
 // We check whether the adress sizeof(meta_data) bytes before is in the linked list
